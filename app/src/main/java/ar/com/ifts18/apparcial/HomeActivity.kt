1bdbdbd21337b6a0687954e795058df22fcc4635
Max-Power1 <ramiro01032003@gmail.com>
2024-10-04 19:06:35 -0300

    funcionalidad de los botones salir y ver historial

--- app/src/main/java/ar/com/ifts18/apparcial/HomeActivity.kt
+++ app/src/main/java/ar/com/ifts18/apparcial/HomeActivity.kt
@@ -1,6 +1,9 @@
 package ar.com.ifts18.apparcial
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity: AppCompatActivity() {
@@ -8,7 +11,20 @@ class HomeActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // aqui va el codigo
+        val botonHistorial = findViewById<Button>(R.id.btVerHistorial)
+        val botonSalir = findViewById<Button>(R.id.btSalir)
 
+        botonHistorial.setOnClickListener{ irAHistorial() }
+        botonSalir.setOnClickListener{ irAInicio() }
+
+        // aqui va el codigo
     }
+    fun irAHistorial(){
+        val intent = Intent(this, HistorialActivity::class.java)
+        startActivity(intent) }
+
+    fun irAInicio(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent) }
+
 }
