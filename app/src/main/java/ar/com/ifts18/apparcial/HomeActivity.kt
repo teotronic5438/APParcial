25cd77bb292e2050c9b65a2f9b04dee1a964c8ef
Max-Power1 <ramiro01032003@gmail.com>
2024-10-06 16:38:40 -0300

    agregado de los botones

--- app/src/main/java/ar/com/ifts18/apparcial/HomeActivity.kt
+++ app/src/main/java/ar/com/ifts18/apparcial/HomeActivity.kt
@@ -3,6 +3,7 @@ package ar.com.ifts18.apparcial
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -11,11 +12,18 @@ class HomeActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val tvNumero1 = findViewById<TextView>(R.id.tvNumero1)
+
+        // Mostrar los datos
+       // val displayText = "Nombre: $nombre\nApellido: $apellido\nCorreo: $correo"
+
+
+/*PRUEBAAAA
         val botonHistorial = findViewById<Button>(R.id.btVerHistorial)
         val botonSalir = findViewById<Button>(R.id.btSalir)
-
-        botonHistorial.setOnClickListener{ irAHistorial() }
-        botonSalir.setOnClickListener{ irAInicio() }
+*/
+       // botonHistorial.setOnClickListener{ irAHistorial() }
+      //  botonSalir.setOnClickListener{ irAInicio() }
 
         // aqui va el codigo
     }
@@ -27,4 +35,7 @@ class HomeActivity: AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent) }
 
+    private fun mostrarToast(mensaje: String) {
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+    }
 }
